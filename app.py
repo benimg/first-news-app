d5066ccb592a5db0cd8e5b9e9d0b37520bb30de9
benimg <moralesgallegos@live.com>
2018-06-06 18:03:58 -0500

    Made a map on the detail page

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 import csv
 from flask import Flask
+from flask import abort
 from flask import render_template
 app = Flask(__name__)
 def get_csv():
@@ -31,5 +32,6 @@ def detail(row_id):
     for row in object_list:
         if row['id'] == row_id:
             return render_template(template, object=row)
+    abort(404)
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
