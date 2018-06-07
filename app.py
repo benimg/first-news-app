a2de2c64e9000b9f50cfd7f85f9a7a2fb2683251
benimg <moralesgallegos@live.com>
2018-06-06 22:15:40 -0500

    Froze my app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from flask import abort
 from flask import render_template
 app = Flask(__name__)
 def get_csv():
-    csv_path = './static/la-riots-deaths.csv'
+    csv_path = './static/nivelsuperior.csv'
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
